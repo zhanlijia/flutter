bfc385f66182fb8c769699207b6ec0b9fdc27c9a
zhanlijia <460377268@qq.com>
2020-05-31 15:39:58 +0800

    生命周期打印

--- lib/BaseWidget.dart
+++ lib/BaseWidget.dart
@@ -5,18 +5,53 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    print("--- base initState");
+    print("+++ $this initState");
+  }
+
+  @override
+  void reassemble() {
+    // TODO: implement reassemble
+    super.reassemble();
+    print("+++ $this reassemble");
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print("+++ $this deactivate");
+  }
+
+  @override
+  void didUpdateWidget(T oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("+++ $this didUpdateWidget");
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print("+++ $this didChangeDependencies");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("+++ $this dispose");
   }
 }
 
 abstract class BaseStatefulWidget extends StatefulWidget {
   BaseStatefulWidget({Key key}) : super(key: key){
-    print("--- BaseStatefulWidget");
+    print("+++ $this");
   }
 }
 
 abstract class BaseStatelessWidget extends StatelessWidget {
    BaseStatelessWidget({Key key}) : super(key: key){
-     print("--- BaseStatelessWidget");
+     print("+++ $this");
    }
 }
