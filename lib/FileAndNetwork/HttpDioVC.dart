be7a4fa5150278c4c4bc94162ecc95ab2b36c791
zhanlijia <460377268@qq.com>
2020-07-11 15:20:44 +0800

    http dio

--- lib/FileAndNetwork/HttpDioVC.dart
+++ lib/FileAndNetwork/HttpDioVC.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
 import 'package:testflutter/BaseWidget.dart';
 
 class HttpDioVC extends BaseStatefulWidget {
@@ -11,12 +13,38 @@ class HttpDioVC extends BaseStatefulWidget {
 }
 
 class _HttpDioVCState extends BaseState<HttpDioVC> {
+  Dio dio = Dio();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text("HttpDio"),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: dio.get("https://api.github.com/orgs/flutterchina/repos"),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              Response response = snapshot.data;
+              var res = response.data;
+              return ListView.builder(
+                itemCount: res.length,
+                itemBuilder: (content, index) {
+                  var item = res[index]["full_name"];
+                  return SimpleDialogOption(
+                    child: Text(item),
+                  );
+                },
+              );
+            }
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
